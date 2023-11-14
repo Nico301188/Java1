66429a1420a3035013db94f890f26ee6357746b4
Nico301188 <nico301188@gmail.com>
2023-11-14 10:30:26 -0300

    claase js

---
+++ JSenHTML/dom.js
@@ -0,0 +1,17 @@
+
+
+// console.log(document.getElementById("mainTitle"));
+const title = document.getElementById("mainTitle");
+ title.innerHTML="hola mundo!"
+
+ console.log(document.getElementById("myDiv").innerHTML)
+ console.log(document.getElementById("myDiv").innerText)
+
+ const element={
+    id:"string",
+    class:"css",
+    name:"name",
+    style:"string",
+    innerHTML:"contenido de HTML",
+    innerText:"Contenido de texto"
+ }
--- Java/Java/example5b.js
+++ Java/Java/example5b.js
@@ -1,10 +1,11 @@
 //ciclos for anidados
 
-const carsQuantity = 50
+const carsQuantity = 5
 const peopleByCar = 2
+var variable = "Global" // la voy a poder leer y escribir en cualquier espacio del codigo.
 
 for (let i = 0; i <= carsQuantity; i++) {
-    const car = "Auto #"
+    const car = "Auto #" // la variable const son de tipo local
     const carWithNumber = car.concat(i)
     let carWithPeople = carWithNumber
     for (let y = 0; y < peopleByCar; y++) {
@@ -12,4 +13,21 @@ for (let i = 0; i <= carsQuantity; i++) {
 
     }
     console.log(carWithPeople)
+}
+const cantGatos = 10;
+for (let i = 1; i<= cantGatos; i++) {
+    const modulo = i % 3
+    let catNumber = `Gato #${i}`
+    switch (modulo) {
+        case 1:
+            catNumber = catNumber.concat("👦")
+            break
+        case 2:
+            catNumber = catNumber.concat("x2")
+            break;
+        default:
+            catNumber = catNumber.concat("x3")
+            break
+    }
+    console.log(catNumber)
 }
